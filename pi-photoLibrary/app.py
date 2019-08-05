21240e1d562ca2489954f8da673ac2ca75829096
Nathan Strong <nathanstrong.fx@gmail.com>
2019-08-04 20:50:53 -0700

    testing firestore api

--- pi-photoLibrary/app.py
+++ pi-photoLibrary/app.py
@@ -1,158 +1,55 @@
 import platform
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
+cred = credentials.Certificate('/googleCredentials/googleCredentials.json')
+firebase_admin.initialize_app(cred)
 
-def checkPostedData(postedData, functionName):
-    if (functionName == "add" or functionName == "subtract" or functionName == "multiply"):
-        if "x" not in postedData or "y" not in postedData:
-            return 301  # Missing parameter
+db = firestore.client()
+
+
+def checkPostedData(postedData, requiredFields):
+    for field in requiredFields:
+        if field not in postedData:
+            return {'code': 301, 'message': f'Field named {field} not found in post.'}
         else:
-            return 200
-    elif (functionName == "division"):
-        if "x" not in postedData or "y" not in postedData:
-            return 301
-        elif int(postedData["y"]) == 0:
-            return 302
-        else:
-            return 200
+            return {'code': 200}
 
 
-class Add(Resource):
+class AddPhoto(Resource):
     def post(self):
-        # If I am here, then the resouce Add was requested using the method POST
-
-        # Step 1: Get posted data:
         postedData = request.get_json()
-
-        # Steb 1b: Verify validity of posted data
-        status_code = checkPostedData(postedData, "add")
-        if (status_code != 200):
+        requiredFields = ['photoName']
+        statusDict = checkPostedData(postedData, requiredFields)
+        if (statusDict['code'] != 200):
             retJson = {
-                "Message": "An error happened",
-                "Status Code": status_code
+                "Message": statusDict['message'],
+                "Status Code": str(statusDict['code'])
             }
             return jsonify(retJson)
 
-        # If i am here, then status_code == 200
-        x = postedData["x"]
-        y = postedData["y"]
-        x = int(x)
-        y = int(y)
+        nameOfPhoto = postedData["photoName"]
 
-        # Step 2: Add the posted data
-        ret = x + y
+        doc_ref = db.collection(u'photos').document(nameOfPhoto)
+        doc_ref.set({
+            u'pathToFile': f'/photos/{nameOfPhoto}.jpg',
+        })
+
         retMap = {
-            'Message': ret,
+            'Message': 'cool',
             'Status Code': 200
         }
         return jsonify(retMap)
 
 
-class Subtract(Resource):
-    def post(self):
-        # If I am here, then the resouce Subtract was requested using the method POST
-
-        # Step 1: Get posted data:
-        postedData = request.get_json()
-
-        # Steb 1b: Verify validity of posted data
-        status_code = checkPostedData(postedData, "subtract")
-
-        if (status_code != 200):
-            retJson = {
-                "Message": "An error happened",
-                "Status Code": status_code
-            }
-            return jsonify(retJson)
-
-        # If i am here, then status_code == 200
-        x = postedData["x"]
-        y = postedData["y"]
-        x = int(x)
-        y = int(y)
-
-        # Step 2: Subtract the posted data
-        ret = x - y
-        retMap = {
-            'Message': ret,
-            'Status Code': 200
-        }
-        return jsonify(retMap)
-
-
-class Multiply(Resource):
-    def post(self):
-        # If I am here, then the resouce Multiply was requested using the method POST
-
-        # Step 1: Get posted data:
-        postedData = request.get_json()
-
-        # Steb 1b: Verify validity of posted data
-        status_code = checkPostedData(postedData, "multiply")
-
-        if (status_code != 200):
-            retJson = {
-                "Message": "An error happened",
-                "Status Code": status_code
-            }
-            return jsonify(retJson)
-
-        # If i am here, then status_code == 200
-        x = postedData["x"]
-        y = postedData["y"]
-        x = int(x)
-        y = int(y)
-
-        # Step 2: Multiply the posted data
-        ret = x * y
-        retMap = {
-            'Message': ret,
-            'Status Code': 200
-        }
-        return jsonify(retMap)
-
-
-class Divide(Resource):
-    def post(self):
-        # If I am here, then the resouce Divide was requested using the method POST
-
-        # Step 1: Get posted data:
-        postedData = request.get_json()
-
-        # Steb 1b: Verify validity of posted data
-        status_code = checkPostedData(postedData, "division")
-
-        if (status_code != 200):
-            retJson = {
-                "Message": "An error happened",
-                "Status Code": status_code
-            }
-            return jsonify(retJson)
-
-        # If i am here, then status_code == 200
-        x = postedData["x"]
-        y = postedData["y"]
-        x = int(x)
-        y = int(y)
-
-        # Step 2: Multiply the posted data
-        ret = (x * 1.0) / y
-        retMap = {
-            'Message': ret,
-            'Status Code': 200
-        }
-        return jsonify(retMap)
-
-
-api.add_resource(Add, "/add")
-api.add_resource(Subtract, "/subtract")
-api.add_resource(Multiply, "/multiply")
-api.add_resource(Divide, "/division")
+api.add_resource(AddPhoto, "/addPhoto")
 
 
 @app.route('/')
